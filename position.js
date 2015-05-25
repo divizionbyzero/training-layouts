f7f99b3c8d1e9d8a56c6ba8d5af62135f47c3b1d
divizionbyzero <divizionbyzero@bk.ru>
2015-05-26 00:28:08 +0300

    code cleaning, code formatting

--- position.js
+++ position.js
@@ -1,23 +1,23 @@
 /**
  * Created by hele on 21.05.2015.
  */
-$(document).ready(function(){
+$(document).ready(function () {
     $("#but").click(function () {
         var a = $('#sel>option:selected').text();
         var b = $('#div-sel>option:selected').text();
-        var t = $("#top").val()+"px";
-        var bot = $("#bottom").val()+"px";
-        var lef = $("#left").val()+"px";
-        var rig = $("#right").val()+"px";
-        $('#'+b).css({position: a, "top": t});
-        $('#'+b).css({"bottom": bot, "right": rig, "left": lef});
+        var t = $("#top").val() + "px";
+        var bot = $("#bottom").val() + "px";
+        var lef = $("#left").val() + "px";
+        var rig = $("#right").val() + "px";
+        $('#' + b).css({position: a, "top": t});
+        $('#' + b).css({"bottom": bot, "right": rig, "left": lef});
 
     });
     $("li[id^='lin'],li[id^='rad'] ").hover(
         function () {
-      $(this).html("<span>"+$(this).css("background-image")+"</span>");
+            $(this).html("<span>" + $(this).css("background-image") + "</span>");
             //$(this).css("background-color","rgba(150,150,150,0.7)");
-},
+        },
         function () {
             $(this).text("");
         });

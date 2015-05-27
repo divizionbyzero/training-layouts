7efa522be7680371d6d084ed5833e90dd8dedc6b
divizionbyzero <divizionbyzero@bk.ru>
2015-05-27 14:15:20 +0300

    ahax training

---
+++ table.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".loadjson").click(function(){
+        $.ajax({
+            url: "table.json",
+            dataType: "json",
+            data: {get_param: 'value'},
+            success: function (data) {
+                $.each(data, function(){
+                    $("tbody").append("<tr><td>"+this.name+"</td><td>"+ this.surname +"</td><td>" + this.id +"</td><td>"+this.city+"</td></tr>");
+                })
+            }
+        })
+    })
+});
